340cfb5539a768f8f744cf0652fd8a29a005f0cd
Stefan Caras <stefan.caras@gmail.com>
2023-03-12 18:27:53 +0200

    update

--- 02_progress_steps/script.js
+++ 02_progress_steps/script.js
@@ -2,12 +2,12 @@ const $ = (query) => document.querySelector(query);
 const items = document.querySelectorAll(".item");
 
 let active = 0;
-const percent = {
-  0: "0%",
-  1: "33%",
-  2: "66%",
-  3: "100%",
-};
+const percent = ["0%"];
+const lineSplit = 100 / (items.length - 1);
+
+for (let i = 1; i < items.length; i++) {
+  percent.push(`${lineSplit * i}%`);
+}
 
 const color = () => {
   $(".blue-line").style.width = percent[active];
@@ -15,7 +15,7 @@ const color = () => {
   if (active === 0) $(".prev").disabled = true;
   else $(".prev").disabled = false;
 
-  if (active === 3) $(".next").disabled = true;
+  if (active === items.length - 1) $(".next").disabled = true;
   else $(".next").disabled = false;
 };
 
